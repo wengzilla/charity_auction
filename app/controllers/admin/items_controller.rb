5469cf7e9233481c9655fb3ed347169aeb754df8
Edward Weng <edweng@gmail.com>
2015-04-20 18:21:38 -0400

    Changing admin panels

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,14 +8,26 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    if Item.create(item_create_params)
+    if Item.create(item_params)
+      redirect_to admin_items_path
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    render 'new'
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item && @item.update_attributes(item_params)
       redirect_to admin_items_path
     end
   end
 
   private
 
-  def item_create_params
+  def item_params
     params.require(:item).permit(:title, :caption, :donor, :starting_price, :photo_url)
   end
 end
